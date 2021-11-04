df3edfac1353a8cc86b9b03c0261b7b3ac75d095
Bastiaan Schaap <bastiaan.schaap@nedap.com>
2021-11-04 17:11:08 +0100

    Return exit code

--- lib/communicator.rb
+++ lib/communicator.rb
@@ -1,21 +1,31 @@
 module VagrantPlugins
   module VagrantDummyCommunicator
     class Communicator < Vagrant.plugin("2", :communicator)
-      # We can successfully ignore everything, no problem
-      def self.match?(machine)
-        true
-      end
 
       def initialize(machine)
+        @env = machine.env
       end
 
       def ready?
+        sleep 2
         true
       end
 
       def wait_for_ready(duration)
         sleep 2
         return true
+      end
+
+      def execute(command, opts=nil)
+        0
+      end
+
+      def sudo(command, opts=nil)
+        execute(command, opts)
+      end
+
+      def test(command, opts=nil)
+        execute(command, opts)
       end
     end
   end
